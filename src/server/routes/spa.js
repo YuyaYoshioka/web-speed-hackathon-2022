b7e8e7c11153b0705fe0b805843c5164122d0ad9
YuyaYoshioka <yuya3330930@yahoo.co.jp>
2022-11-21 00:27:45 +0900

    Brotliで圧縮

--- src/server/routes/spa.js
+++ src/server/routes/spa.js
@@ -1,14 +1,27 @@
 import { join } from "path";
 
-import compression from "fastify-compress";
 import fastifyStatic from "fastify-static";
 
 /**
  * @type {import('fastify').FastifyPluginCallback}
  */
 export const spaRoute = async (fastify) => {
-  fastify.register(compression);
+  fastify.addHook("onSend", function (req, reply, payload, next) {
+    const filenames = payload.filename.split("/");
+    const filename = filenames[filenames.length - 1];
+    const names = filename.split(".");
+    const br = names[names.length - 1];
+    if (br === "br") {
+      reply.header("content-encoding", "br");
+      const name = names[names.length - 2];
+      if (name === "js") {
+        reply.header("content-type", "application/javascript; charset=utf8");
+      }
+    }
+    next();
+  });
   fastify.register(fastifyStatic, {
+    preCompressed: true,
     root: join(__dirname, "public"),
     wildcard: false,
   });
